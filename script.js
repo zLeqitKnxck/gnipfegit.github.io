a47611d018ccd228a0250948867a04c95ffd3c84
gnipfeGIT <104355283+gnipfeGIT@users.noreply.github.com>
2022-05-17 16:09:08 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,137 @@
+onload = ()=> {
+  canvas = document.querySelector('canvas'),
+  context = canvas.getContext('2d');
+  if (!!context) {
+      /* if canvas is supported */
+  }
+};
+
+button = document.querySelector('button');
+button.addEventListener('click', function() { 
+    /* A variable to track the drawing intervals */
+    n = 0, 
+    /* Interval speed for the animation */
+    loadingInterval = setInterval(drawLoader, 60); 
+});
+
+/* Style of the lines of the square that'll be drawn */
+let gradient = context.createLinearGradient(0, 0, 32, 32);
+gradient.addColorStop(0, '#c7f0fe');
+gradient.addColorStop(1, '#56d3c9');
+context.strokeStyle = gradient;
+context.lineWidth = 8;
+
+function drawLoader() {
+  with(context) {
+    clearRect(0, 0, 32, 32);
+    beginPath();
+    /* Up to 25% */
+    if (n<=25){ 
+      /*
+        (0,0)-----(32,0)
+      */
+      // code to draw the top line, incrementally
+    }
+    /* Between 25 to 50 percent */
+    else if(n>25 && n<=50){ 
+      /*
+        (0,0)-----(32,0)
+                  |
+                  |
+                  (32,32)
+      */
+      // code to draw the top and right lines.
+    }
+    /* Between 50 to 75 percent */
+    else if(n>50 && n<= 75){ 
+      /*
+        (0,0)-----(32,0)
+                  |
+                  |
+        (0,32)----(32,32)
+      */
+      // code to draw the top, right and bottom lines.
+    }
+      /* Between 75 to 100 percent */
+    else if(n>75 && n<=100){
+      /*
+        (0,0)-----(32,0)
+            |      |
+            |      |
+        (0,32)----(32,32)
+      */
+      // code to draw all four lines of the square.
+    }
+    stroke();
+  }
+  // Convert the Canvas drawing to PNG and assign it to the favicon
+  favicon.href = canvas.toDataURL('image/png');
+  /* When finished drawing */
+  if (n === 100) {
+    clearInterval(loadingInterval);
+    return;
+  }
+  // Increment the variable used to keep track of the drawing intervals
+  n++;
+}
+
+function drawLoader() {
+  with(context) {
+    clearRect(0, 0, 32, 32);
+    beginPath();
+    /* Up to 25% of the time assigned to draw */
+    if (n<=25){ 
+      /*
+        (0,0)-----(32,0)
+      */
+      moveTo(0, 0); lineTo((32/25)*n, 0);
+    }
+    /* Between 25 to 50 percent */
+    else if(n>25 && n<=50){ 
+      /*
+        (0,0)-----(32,0)
+                  |
+                  |
+                  (32,32)
+      */
+      moveTo(0, 0); lineTo(32, 0);
+      moveTo(32, 0); lineTo(32, (32/25)*(n-25));
+    }
+    /* Between 50 to 75 percent */
+    else if(n>50 && n<= 75){ 
+      /*
+        (0,0)-----(32,0)
+                  |
+                  |
+        (0,32)----(32,32)
+      */
+      moveTo(0, 0); lineTo(32, 0);
+      moveTo(32, 0); lineTo(32, 32);
+      moveTo(32, 32); lineTo(-((32/25)*(n-75)), 32);
+    }
+      /* Between 75 to 100 percent */
+    else if(n>75 && n<=100){
+      /*
+        (0,0)-----(32,0)
+            |      |
+            |      |
+        (0,32)----(32,32)
+      */
+      moveTo(0, 0); lineTo(32, 0);
+      moveTo(32, 0); lineTo(32, 32);
+      moveTo(32, 32); lineTo(0, 32);
+      moveTo(0, 32); lineTo(0, -((32/25)*(n-100)));
+    }
+    stroke();
+  }
+
+  // Convert the Canvas drawing to PNG and assign it to the favicon
+  favicon.href = canvas.toDataURL('image/png');
+  /* When finished drawing */
+  if (n === 100) {
+      clearInterval(loadingInterval);
+      return;
+  }
+  // Increment the variable used to keep track of drawing intervals
+  n++;
+}
